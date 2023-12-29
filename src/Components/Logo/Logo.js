3f36e674cab16ecc4e7456f648bb5f736799a6e1
SGRAVANI <shakuntla.ravani@gmail.com>
2023-12-29 10:21:09 +0530

    first commit

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -3,7 +3,7 @@ import styles from "./Logo.module.css";
 export default function Logo({className}) {
   return (
     <div>
-        <img src={require("../../assets/logo.png")} className={ className?className:styles.logo}/>
+        <a href="#"><img src={require("../../assets/logo.png")} className={ className?className:styles.logo}/></a>
     </div>
   )
 }
